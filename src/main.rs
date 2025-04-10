421cc0e8493de9ef1b7aefcccfece692a2636f4b
Jacek Karaskiewicz <jacek.karaskiewicz@gmail.com>
2025-04-10 21:38:42 +0200

    Update

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,7 @@ use configs::execute;
 
 fn main() {
     let output = execute().unwrap_or_else(|e| {
-        let backtrace = e.backtrace();
-        eprintln!("Error: {e}\n{}", backtrace);
+        eprintln!("Error: {e}");
         std::process::exit(1);
     });
     if !output.is_empty() {
